1f80ed9367e71ddf2062a3d74a44d3096a83b9c4
panweiwei <panweiwei.pww@mapgis.com>
2026-05-16 21:14:43 +0800

    feat: rich content length limit check

--- src/common/constants/content-limits.ts
+++ src/common/constants/content-limits.ts
@@ -1,15 +1,15 @@
 ﻿export const CONTENT_BYTE_LIMITS = {
   article: {
     min: 10,
-    max: 512 * 1024,
+    max: 3 * 1024 * 1024,
   },
   draft: {
     min: 1,
-    max: 1024 * 1024,
+    max: 3 * 1024 * 1024,
   },
   topic: {
     min: 2,
-    max: 512 * 1024,
+    max: 2 * 1024 * 1024,
   },
   comment: {
     min: 1,
